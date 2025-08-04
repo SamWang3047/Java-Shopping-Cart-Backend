a78a631bee66b9aacfa99364be5061769cd6558b
SamWangDesktop <w304714766@gmail.com>
2025-08-04 14:53:53 +1000

    init

---
+++ src/test/java/com/ShoppingCart/ShoppingCartDemo/ShoppingCartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShoppingCart.ShoppingCartDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
