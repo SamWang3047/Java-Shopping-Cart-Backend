9ee6017e44e61d5917e3f1850543cf38718aab70
SamWangDesktop <w304714766@gmail.com>
2025-08-06 15:40:57 +1000

    Category and Product

--- src/main/java/com/ShoppingCart/ShoppingCartDemo/model/Product.java
+++ src/main/java/com/ShoppingCart/ShoppingCartDemo/model/Product.java
@@ -3,7 +3,27 @@ package com.ShoppingCart.ShoppingCartDemo.model;
 import java.math.BigDecimal;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String brand;
@@ -12,7 +32,12 @@ public class Product {
     private String description;
     private int inventory;
     
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
     private Category category;
+
+    
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Image> images;
     
 
